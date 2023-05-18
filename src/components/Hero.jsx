65d07f44f8f29b3f3dfd206fd3bb15569726de34
glaucc <ahmedovyusif8785@gmail.com>
2023-05-19 03:44:58 +0400

    some data processing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Joseffron</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I make web applications feel comfortable by finding & fixing their vulnerabilities<br className='sm:block hidden' />
+            to provide <b>security</b>
           </p>
         </div>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,26 +4,18 @@ import {
   creator,
   web,
   javascript,
-  typescript,
   html,
   css,
   reactjs,
-  redux,
   tailwind,
-  nodejs,
-  mongodb,
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
   carrent,
   jobit,
   tripguide,
   threejs,
-  
+
 } from "../assets";
 
 export const navLinks = [
